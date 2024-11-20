4318a94a3d48c8a3938b9a8adee0d6c2fefa684a
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-11-20 13:14:47 +0200

    Add files via upload

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Markus";
+            var number = 0;
+            int number2 = 1;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(number2);
+        }
+    }
+}
