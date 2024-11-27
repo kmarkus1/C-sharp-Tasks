e5a5bf2e377d88609c769a77e02212b14e2c7dbc
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-11-27 11:56:21 +0200

    Add files via upload

---
+++ C#Uppgifter/DaysToChristmas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagarTillJul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            DateTime christmas = new DateTime(2024, 12, 25);
+
+            
+
+            if (today > christmas)
+            {
+                
+                christmas = christmas.AddYears(1);
+            }
+            
+            Console.WriteLine($"There are {(christmas.Date - today.Date).Days} days until Christmas!");
+
+            
+        }
+    }
+}
