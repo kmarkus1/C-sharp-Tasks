59b6e7f787b8fac3ef89687f083dc4be4005fb1d
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-11-27 11:59:05 +0200

    Add files via upload

---
+++ C#Uppgifter/ShoppingList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Add item to shopping list or type 's' to stop: ");
+                string item = Console.ReadLine();
+
+                if (item == "s")
+                {
+                    break;
+                }
+                shoppingList.Add(item);
+            }
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.Write($"{shoppingList[i]} ");
+            }
+        }
+    }
+}
