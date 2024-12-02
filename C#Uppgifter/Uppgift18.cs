2b7842b650c13f6f2ec6c6d221406792aa9b412d
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-12-02 13:48:07 +0200

    Add files via upload

---
+++ C#Uppgifter/Uppgift18.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift18
+{
+    internal class Program
+    {
+        static int Triangle(int width, int height)
+        {
+            return width * height;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The area of the triangle is {Triangle(width, height)}");
+        }
+    }
+}
