5e6306be64e054152c558435cc94891684d71be6
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-12-03 12:03:50 +0200

    Add files via upload

---
+++ C#Uppgifter/Uppgift20.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift20
+{
+    class Circle
+    {
+        public double Radius;
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Circle radius = new Circle();
+            radius.Radius = 1.5;
+            Console.WriteLine(radius.Radius);
+            Console.WriteLine($"Area of the circle is: {radius.CalculateArea()}");
+        }
+    }
+}
