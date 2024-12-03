daf057aff8634357f24e9c15ed1d107b43a0d4f9
kmarkus1 <167196765+kmarkus1@users.noreply.github.com>
2024-12-03 12:20:31 +0200

    Add files via upload

---
+++ C#Uppgifter/Uppgift23.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.Write("Input here: ");
+            string mainInput = Console.ReadLine();
+
+            try
+            {
+                number = Convert.ToInt32(mainInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("error");
+            }
+
+            bool parse = int.TryParse(mainInput, out number);
+
+            if (parse)
+            {
+                Console.WriteLine("it worked");
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("did not work");
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
